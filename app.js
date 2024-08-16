a2e6bbba50cfb45dc529c2ca768f42ce62845040
SadaaqueAli <sadaaqueali@gmail.com>
2024-08-16 23:39:19 +0500

    html css created

---
+++ app.js
@@ -0,0 +1,21 @@
+let direcion = { x: 0, y: 0 };
+const foodSound = new Audio('food.mp3');
+const gameOver = new Audio('gameover.mp3');
+const moveSound = new Audio('move.mp3');
+const musicSound = new Audio('music.mp3');
+
+//game function
+function main(ctime) {
+    window.requestAnimationFrame(main)
+    console.log(ctime)
+}
+
+
+
+
+
+
+
+
+//logic starts here
+window.requestAnimationFrame(main)
