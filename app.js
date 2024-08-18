532135c3bdea51aa0c65cde330ac35690139425e
SadaaqueAli <sadaaqueali@gmail.com>
2024-08-18 16:54:22 +0500

    completed now game is ready

--- app.js
+++ app.js
@@ -44,12 +44,19 @@ function gameEngine() {
         inputDir = { x: 0, y: 0 };
         alert('Game over ! press any for play again')
         snakeArr = [{ x: 13, y: 15 }];
-        musicSound.play();
+        // musicSound.play();
         score = 0;
     }
     // agr sanke ne food ko kha lia or score barha phr dubara food show hona chiye
     if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
         foodSound.play();
+        score += 1;
+        if (score > hiscoreval) {
+            hiscoreval = score;
+            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+            hiScoreBox.innerHTML = "HiScore: " + hiscoreval;
+        }
+        scoreBox.innerHTML = "score: " + score;
         snakeArr.unshift({ x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y })
         let a = 2;
         let b = 16;
@@ -77,38 +84,42 @@ function gameEngine() {
         board.appendChild(snakeElement);
     })
     //display food
-        foodElement = document.createElement('div')
-        foodElement.style.gridRowStart = food.y;
-        foodElement.style.gridColumnStart = food.x;
-        foodElement.classList.add('food')
-        board.appendChild(foodElement);
-    }
+    foodElement = document.createElement('div')
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food')
+    board.appendChild(foodElement);
+}
 
 
 
 //logic starts here
+let hiscore = localStorage.getItem("hiscore");
+if (hiscore === null) {
+    let hiscoreval = 0;
+    localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+} else {
+    hiscoreval = JSON.parse(hiscore);
+    hiScoreBox.innerHTML = "HiScore: " + hiscore;
+}
 window.requestAnimationFrame(main)
 window.addEventListener('keydown', e => {
     inputDir = { x: 0, y: 1 }
     moveSound.play();
     switch (e.key) {
         case "ArrowUp":
-            console.log("ArrowUp")
             inputDir.x = 0;
             inputDir.y = -1;
             break;
         case "ArrowDown":
-            console.log("ArrowDown")
             inputDir.x = 0;
             inputDir.y = 1;
             break;
         case "ArrowLeft":
-            console.log("ArrowLeft")
             inputDir.x = -1;
             inputDir.y = 0;
             break;
         case "ArrowRight":
-            console.log("ArrowRight")
             inputDir.x = 1;
             inputDir.y = 0;
             break;
